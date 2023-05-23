039c29ddcb88235d8c0049638adacd26dcd71edc
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-05-23 18:49:09 +0300

    kategorilere göre filitreleme islemi

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,12 +13,27 @@ const Header = ({ data, setData }) => {
     newArr.sort((a, b) => b.price - a.price);
     setData(newArr);
   };
+  const kadin = "women's clothing";
+  const erkek = "men's clothing";
+  const aksesuar = "jewelery";
+  const elektronik = "electronics";
+
+  const category = (categoryName) => {
+    const newArr = [...data];
+
+    setData(newArr.filter((item) => item.category === categoryName));
+  };
 
   return (
     <div className=" border border-bottom p-3">
       <h1>Shop</h1>
       <div className="d-flex justify-content-between p-2">
-        <div>kategori</div>
+        <div>
+          <button onClick={() => category(kadin)}>Kadın</button>
+          <button onClick={() => category(erkek)}>Erkek</button>
+          <button onClick={() => category(aksesuar)}>Aksesuar</button>
+          <button onClick={() => category(elektronik)}>Elektronik</button>
+        </div>
         <div>
           <button onClick={artan}>artan</button>
           <button onClick={azalan}>azalan</button>
